20dd7b28651c9c143d09ca9010042778a7b799a4
Grvzard <grvzard@outlook.com>
2023-06-18 20:43:40 +0800

    feat(server): support "hypercorn" as asgi server

--- xxdb/http/server.py
+++ xxdb/http/server.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from typing import Coroutine
+from functools import partial
 
 import toml
 import uvicorn
+import hypercorn
+from hypercorn.asyncio import serve
 
 from .app import create_app, AppConfig
 
@@ -11,12 +14,26 @@ class Server:
     def __init__(self, config_path: str):
         self.config = toml.load(Path(config_path).open('r'))
         _app = create_app(AppConfig(**self.config['app']))
-        if "host" not in self.config['server']:
-            self.config['server']['host'] = "0.0.0.0"
-        _uvicorn_config = uvicorn.Config(_app, **self.config['server'])
-        self._server = uvicorn.Server(_uvicorn_config)
+
+        self.config['server'].setdefault('host', "0.0.0.0")
+        self.config['server'].setdefault('port', 7791)
+
+        server_type = self.config['server'].pop('type', "uvicorn")
+
+        if server_type == 'uvicorn':
+            _config = uvicorn.Config(_app, **self.config['server'])
+            self._serve = uvicorn.Server(_config).serve
+
+        elif server_type == 'hypercorn':
+            _config = hypercorn.Config.from_mapping(self.config['server'])
+            _config.bind = [f"{_config.host}:{_config.port}"]
+            self._serve = partial(serve, _app, _config)
+
+        else:
+            raise Exception(f"unknown server type: {server_type}")
 
         self.debug = self.config['server'].get('reload', False)
 
     def serve(self) -> Coroutine:
-        return self._server.serve()
+        # return self._server.serve()
+        return self._serve()
